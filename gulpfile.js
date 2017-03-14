82ee864839ea292e58099d70152cebe3c8e68f45
Joshua Udensi <AJUdensi@Andelas-MacBook-Pro-59.local>
2017-03-14 09:05:43 +0100

    chore(gulpfile.js, package.json): setup gulp taskrunner \n convert from grunt to gulp taskrunner \n [Finishes #141404643]

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+const nodemon = require('gulp-nodemon');
+const jshint = require('gulp-jshint');
+
+gulp.task('browser-sync', ['nodemon'], () => {
+  browserSync({
+    proxy: 'localhost:3000',
+    port: 5000,
+    notify: true,
+    logLevel: 'debug',
+    logPrefix: 'C4H-kakashi',
+    logFileChanges: true,
+    logConnections: true,
+    minify: true,
+  });
+});
+
+gulp.task('nodemon', ['lint'], (cb) => {
+  let called = false;
+  return nodemon({
+    script: 'server.js',
+    ignore: [
+      'node_modules/',
+    ],
+  })
+  .on('start', () => {
+    if (!called) {
+      called = true;
+      cb();
+    }
+  })
+  .on('restart', () => {
+    setTimeout(() => {
+      reload({ stream: false });
+    }, 1000);
+  });
+});
+
+gulp.task("lint", function() {
+    gulp.src(['*.js', 'test/**/*.js', 'public/js/*.js', 'public/js/**/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter("default"));
+});
+
+gulp.task('default', ['browser-sync'], () => {
+  gulp.watch(['public/js/*.js'], reload);
+  gulp.watch(['public/js/**/*.js'], reload);
+  gulp.watch(['**/*.js'], reload);
+  gulp.watch(['public/css/*.*'], reload);
+  gulp.watch(['public/views/*.html'], reload);
+  gulp.watch(['*.js'], reload);
+});
