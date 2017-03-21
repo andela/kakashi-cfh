214ca6136b974ab934970a58aae2edfe068eb54f
andela-judensi <joshua.udensi@andela.com>
2017-03-21 16:31:54 +0100

    initate invite-develop for pair programming

--- config/routes.js
+++ config/routes.js
@@ -1,93 +1,116 @@
 var async = require('async');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+mongoose.Promise = global.Promise;
 
 module.exports = function(app, passport, auth) {
     //User Routes
-    var users = require('../app/controllers/users');
-    app.get('/signin', users.signin);
-    app.get('/signup', users.signup);
-    app.get('/chooseavatars', users.checkAvatar);
-    app.get('/signout', users.signout);
+  var users = require('../app/controllers/users');
+  app.get('/signin', users.signin);
+  app.get('/signup', users.signup);
+  app.get('/chooseavatars', users.checkAvatar);
+  app.get('/signout', users.signout);
 
     //Setting up the users api
-    app.post('/users', users.create);
-    app.post('/users/avatars', users.avatars);
+  app.post('/users', users.create);
+  app.post('/users/avatars', users.avatars);
 
     // Donation Routes
-    app.post('/donations', users.addDonation);
+  app.post('/donations', users.addDonation);
 
-    app.post('/users/session', passport.authenticate('local', {
-        failureRedirect: '/signin',
-        failureFlash: 'Invalid email or password.'
-    }), users.session);
+  app.post('/users/session', passport.authenticate('local', {
+    failureRedirect: '/signin',
+    failureFlash: 'Invalid email or password.'
+  }), users.session);
 
-    app.get('/users/me', users.me);
-    app.get('/users/:userId', users.show);
+  app.get('/users/me', users.me);
+  app.get('/users/:userId', users.show);
+
+
+    // API routes for user search
+  app.get('/api/search/users', /** Authentication middleware **/ (req, res) => {
+    User.find({}).select('name email').then((allUsers) => {
+      res.send(allUsers);
+    });
+  });
+
+  app.get('/api/search/users/:userid', /** Authentication middleware **/ (req, res) => {
+    const userid = req.params.userid;
+    User.findById(userid, (err, oneUser) => {
+      if (!err) {
+        res.send(oneUser);
+      } else {
+        res.send('An error occurred');
+      }
+    });
+  });
 
     //Setting the facebook oauth routes
-    app.get('/auth/facebook', passport.authenticate('facebook', {
-        scope: ['email'],
-        failureRedirect: '/signin'
-    }), users.signin);
+  app.get('/auth/facebook', passport.authenticate('facebook', {
+    scope: ['email'],
+    failureRedirect: '/signin'
+  }), users.signin);
 
-    app.get('/auth/facebook/callback', passport.authenticate('facebook', {
-        failureRedirect: '/signin'
-    }), users.authCallback);
+  app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    failureRedirect: '/signin'
+  }), users.authCallback);
 
     //Setting the github oauth routes
-    app.get('/auth/github', passport.authenticate('github', {
-        failureRedirect: '/signin'
-    }), users.signin);
+  app.get('/auth/github', passport.authenticate('github', {
+    failureRedirect: '/signin'
+  }), users.signin);
 
-    app.get('/auth/github/callback', passport.authenticate('github', {
-        failureRedirect: '/signin'
-    }), users.authCallback);
+  app.get('/auth/github/callback', passport.authenticate('github', {
+    failureRedirect: '/signin'
+  }), users.authCallback);
 
     //Setting the twitter oauth routes
-    app.get('/auth/twitter', passport.authenticate('twitter', {
-        failureRedirect: '/signin'
-    }), users.signin);
+  app.get('/auth/twitter', passport.authenticate('twitter', {
+    failureRedirect: '/signin'
+  }), users.signin);
 
-    app.get('/auth/twitter/callback', passport.authenticate('twitter', {
-        failureRedirect: '/signin'
-    }), users.authCallback);
+  app.get('/auth/twitter/callback', passport.authenticate('twitter', {
+    failureRedirect: '/signin'
+  }), users.authCallback);
 
     //Setting the google oauth routes
-    app.get('/auth/google', passport.authenticate('google', {
-        failureRedirect: '/signin',
-        scope: [
-            'https://www.googleapis.com/auth/userinfo.profile',
-            'https://www.googleapis.com/auth/userinfo.email'
-        ]
-    }), users.signin);
+  app.get('/auth/google', passport.authenticate('google', {
+    failureRedirect: '/signin',
+    scope: [
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/userinfo.email'
+    ]
+  }), users.signin);
 
-    app.get('/auth/google/callback', passport.authenticate('google', {
-        failureRedirect: '/signin'
-    }), users.authCallback);
+  app.get('/auth/google/callback', passport.authenticate('google', {
+    failureRedirect: '/signin'
+  }), users.authCallback);
 
     //Finish with setting up the userId param
-    app.param('userId', users.user);
+  app.param('userId', users.user);
 
     // Answer Routes
-    var answers = require('../app/controllers/answers');
-    app.get('/answers', answers.all);
-    app.get('/answers/:answerId', answers.show);
+  var answers = require('../app/controllers/answers');
+  app.get('/answers', answers.all);
+  app.get('/answers/:answerId', answers.show);
     // Finish with setting up the answerId param
-    app.param('answerId', answers.answer);
+  app.param('answerId', answers.answer);
 
     // Question Routes
-    var questions = require('../app/controllers/questions');
-    app.get('/questions', questions.all);
-    app.get('/questions/:questionId', questions.show);
+  var questions = require('../app/controllers/questions');
+  app.get('/questions', questions.all);
+  app.get('/questions/:questionId', questions.show);
     // Finish with setting up the questionId param
-    app.param('questionId', questions.question);
+  app.param('questionId', questions.question);
 
     // Avatar Routes
-    var avatars = require('../app/controllers/avatars');
-    app.get('/avatars', avatars.allJSON);
+  var avatars = require('../app/controllers/avatars');
+  app.get('/avatars', avatars.allJSON);
 
     //Home route
-    var index = require('../app/controllers/index');
-    app.get('/play', index.play);
-    app.get('/', index.render);
+  var index = require('../app/controllers/index');
+  app.get('/play', index.play);
+  app.get('/', index.render);
 
 };
--- public/js/controllers/game.js
+++ public/js/controllers/game.js
@@ -1,184 +1,222 @@
 angular.module('mean.system')
-.controller('GameController', ['$scope', 'game', '$timeout', '$location', 'MakeAWishFactsService', '$dialog', function ($scope, game, $timeout, $location, MakeAWishFactsService, $dialog) {
-    $scope.hasPickedCards = false;
-    $scope.winningCardPicked = false;
-    $scope.showTable = false;
-    $scope.modalShown = false;
-    $scope.game = game;
-    $scope.pickedCards = [];
-    var makeAWishFacts = MakeAWishFactsService.getMakeAWishFacts();
-    $scope.makeAWishFact = makeAWishFacts.pop();
+.controller('GameController', ['$scope', 'game', '$timeout', '$location', 'MakeAWishFactsService', '$dialog', '$http', function ($scope, game, $timeout, $location, MakeAWishFactsService, $dialog, $http) {
+  $scope.hasPickedCards = false;
+  $scope.winningCardPicked = false;
+  $scope.showTable = false;
+  $scope.modalShown = false;
+  $scope.game = game;
+  $scope.pickedCards = [];
+  $scope.showFindUsersButton = false;
+  let makeAWishFacts = MakeAWishFactsService.getMakeAWishFacts();
+  $scope.makeAWishFact = makeAWishFacts.pop();
+  const usersToInvite = [];
 
-    $scope.pickCard = function(card) {
-      if (!$scope.hasPickedCards) {
-        if ($scope.pickedCards.indexOf(card.id) < 0) {
-          $scope.pickedCards.push(card.id);
-          if (game.curQuestion.numAnswers === 1) {
-            $scope.sendPickedCards();
-            $scope.hasPickedCards = true;
-          } else if (game.curQuestion.numAnswers === 2 &&
+  $scope.pickCard = (card) => {
+    if (!$scope.hasPickedCards) {
+      if ($scope.pickedCards.indexOf(card.id) < 0) {
+        $scope.pickedCards.push(card.id);
+        if (game.curQuestion.numAnswers === 1) {
+          $scope.sendPickedCards();
+          $scope.hasPickedCards = true;
+        } else if (game.curQuestion.numAnswers === 2 &&
             $scope.pickedCards.length === 2) {
             //delay and send
-            $scope.hasPickedCards = true;
-            $timeout($scope.sendPickedCards, 300);
-          }
-        } else {
-          $scope.pickedCards.pop();
+          $scope.hasPickedCards = true;
+          $timeout($scope.sendPickedCards, 300);
         }
-      }
-    };
-
-    $scope.pointerCursorStyle = function() {
-      if ($scope.isCzar() && $scope.game.state === 'waiting for czar to decide') {
-        return {'cursor': 'pointer'};
       } else {
-        return {};
+        $scope.pickedCards.pop();
       }
-    };
+    }
+  };
 
-    $scope.sendPickedCards = function() {
-      game.pickCards($scope.pickedCards);
-      $scope.showTable = true;
-    };
+  $scope.pointerCursorStyle = () => {
+    if ($scope.isCzar() && $scope.game.state === 'waiting for czar to decide') {
+      return { cursor: 'pointer' };
+    }
+    return {};
+  };
 
-    $scope.cardIsFirstSelected = function(card) {
-      if (game.curQuestion.numAnswers > 1) {
-        return card === $scope.pickedCards[0];
-      } else {
-        return false;
-      }
-    };
+  $scope.sendPickedCards = () => {
+    game.pickCards($scope.pickedCards);
+    $scope.showTable = true;
+  };
 
-    $scope.cardIsSecondSelected = function(card) {
-      if (game.curQuestion.numAnswers > 1) {
-        return card === $scope.pickedCards[1];
-      } else {
-        return false;
-      }
-    };
+  $scope.cardIsFirstSelected = (card) => {
+    if (game.curQuestion.numAnswers > 1) {
+      return card === $scope.pickedCards[0];
+    }
+    return false;
+  };
 
-    $scope.firstAnswer = function($index){
-      if($index % 2 === 0 && game.curQuestion.numAnswers > 1){
-        return true;
-      } else{
-        return false;
-      }
-    };
+  $scope.cardIsSecondSelected = (card) => {
+    if (game.curQuestion.numAnswers > 1) {
+      return card === $scope.pickedCards[1];
+    }
+    return false;
+  };
 
-    $scope.secondAnswer = function($index){
-      if($index % 2 === 1 && game.curQuestion.numAnswers > 1){
-        return true;
-      } else{
-        return false;
-      }
-    };
+  $scope.firstAnswer = ($index) => {
+    if ($index % 2 === 0 && game.curQuestion.numAnswers > 1) {
+      return true;
+    }
+    return false;
+  };
 
-    $scope.showFirst = function(card) {
-      return game.curQuestion.numAnswers > 1 && $scope.pickedCards[0] === card.id;
-    };
+  $scope.secondAnswer = ($index) => {
+    if ($index % 2 === 1 && game.curQuestion.numAnswers > 1) {
+      return true;
+    }
+    return false;
+  };
 
-    $scope.showSecond = function(card) {
-      return game.curQuestion.numAnswers > 1 && $scope.pickedCards[1] === card.id;
-    };
+  $scope.showFirst = card => game.curQuestion.numAnswers > 1 && $scope.pickedCards[0] === card.id;
 
-    $scope.isCzar = function() {
-      return game.czar === game.playerIndex;
-    };
+  $scope.showSecond = card => game.curQuestion.numAnswers > 1 && $scope.pickedCards[1] === card.id;
 
-    $scope.isPlayer = function($index) {
-      return $index === game.playerIndex;
-    };
+  $scope.isCzar = () => game.czar === game.playerIndex;
 
-    $scope.isCustomGame = function() {
-      return !(/^\d+$/).test(game.gameID) && game.state === 'awaiting players';
-    };
+  $scope.isPlayer = $index => $index === game.playerIndex;
 
-    $scope.isPremium = function($index) {
-      return game.players[$index].premium;
-    };
+  $scope.isCustomGame = () => !(/^\d+$/).test(game.gameID) && game.state === 'awaiting players';
 
-    $scope.currentCzar = function($index) {
-      return $index === game.czar;
-    };
+  $scope.isPremium = $index => game.players[$index].premium;
 
-    $scope.winningColor = function($index) {
-      if (game.winningCardPlayer !== -1 && $index === game.winningCard) {
-        return $scope.colors[game.players[game.winningCardPlayer].color];
-      } else {
-        return '#f9f9f9';
-      }
-    };
+  $scope.currentCzar = $index => $index === game.czar;
 
-    $scope.pickWinning = function(winningSet) {
-      if ($scope.isCzar()) {
-        game.pickWinning(winningSet.card[0]);
-        $scope.winningCardPicked = true;
-      }
-    };
+  $scope.winningColor = ($index) => {
+    if (game.winningCardPlayer !== -1 && $index === game.winningCard) {
+      return $scope.colors[game.players[game.winningCardPlayer].color];
+    }
+    return '#f9f9f9';
+  };
 
-    $scope.winnerPicked = function() {
-      return game.winningCard !== -1;
-    };
+  $scope.pickWinning = (winningSet) => {
+    if ($scope.isCzar()) {
+      game.pickWinning(winningSet.card[0]);
+      $scope.winningCardPicked = true;
+    }
+  };
 
-    $scope.startGame = function() {
+  $scope.winnerPicked = () => game.winningCard !== -1;
+
+  $scope.startGame = () => {
+    console.log(game.playerIndex);
+    console.log(game.joinOverride);
+    console.log(game.players.length);
+    console.log(game.playerMinLimit);
+    if ((game.playerIndex === 0 || game.joinOverride) && (game.players.length >= game.playerMinLimit)) {
       game.startGame();
-    };
+    } else if (game.players.length < game.playerMinLimit) {
+      $('#gameModal').modal();
+      $scope.gameInviteMessage = 'The minimum required number of players have not been reached';
+    } else if (game.players.length > game.playerMaxLimit) {
+      $('#gameModal').modal();
+      $scope.gameInviteMessage = 'The maximum required number of players have reached. Bye bye';
+    }
+  };
 
-    $scope.abandonGame = function() {
-      game.leaveGame();
-      $location.path('/');
-    };
+  $scope.findUsers = () => {
+    $http.get('http://localhost:5001/api/search/users/').then((response) => {
+      const users = response.data;
+      $scope.availableUsers = users;
+      $('#availableUsers').modal();
+    }, () => {
+      console.log('failures');
+    });
+  };
+
+  $scope.sendInvites = () => {
+    const gameUrl = window.location.href;
+    console.log(gameUrl);
+    console.log(usersToInvite);
+  };
+
+  $scope.addToInviteList = (userToInvite) => {
+    if (usersToInvite.indexOf(userToInvite) === -1) {
+      usersToInvite.push(userToInvite);
+    } else {
+      usersToInvite.splice(userToInvite, 1);
+    }
+  };
+
+  $scope.abandonGame = () => {
+    game.leaveGame();
+    $location.path('/');
+  };
 
     // Catches changes to round to update when no players pick card
     // (because game.state remains the same)
-    $scope.$watch('game.round', function() {
-      $scope.hasPickedCards = false;
-      $scope.showTable = false;
-      $scope.winningCardPicked = false;
-      $scope.makeAWishFact = makeAWishFacts.pop();
-      if (!makeAWishFacts.length) {
-        makeAWishFacts = MakeAWishFactsService.getMakeAWishFacts();
-      }
-      $scope.pickedCards = [];
-    });
+  $scope.$watch('game.round', () => {
+    $scope.hasPickedCards = false;
+    $scope.showTable = false;
+    $scope.winningCardPicked = false;
+    $scope.makeAWishFact = makeAWishFacts.pop();
+    if (!makeAWishFacts.length) {
+      makeAWishFacts = MakeAWishFactsService.getMakeAWishFacts();
+    }
+    $scope.pickedCards = [];
+  });
 
     // In case player doesn't pick a card in time, show the table
-    $scope.$watch('game.state', function() {
-      if (game.state === 'waiting for czar to decide' && $scope.showTable === false) {
-        $scope.showTable = true;
-      }
-    });
+  $scope.$watch('game.state', () => {
+    if (game.state === 'waiting for czar to decide' && $scope.showTable === false) {
+      $scope.showTable = true;
+    }
+  });
 
-    $scope.$watch('game.gameID', function() {
-      if (game.gameID && game.state === 'awaiting players') {
-        if (!$scope.isCustomGame() && $location.search().game) {
+  $scope.$watch('game.gameID', () => {
+    if (game.gameID && game.state === 'awaiting players') {
+      if (!$scope.isCustomGame() && $location.search().game) {
           // If the player didn't successfully enter the request room,
           // reset the URL so they don't think they're in the requested room.
-          $location.search({});
-        } else if ($scope.isCustomGame() && !$location.search().game) {
+        console.log('resetting url');
+        $location.search({});
+      } else if ($scope.isCustomGame() && !$location.search().game) {
           // Once the game ID is set, update the URL if this is a game with friends,
           // where the link is meant to be shared.
-          $location.search({game: game.gameID});
-          if(!$scope.modalShown){
-            setTimeout(function(){
-              var link = document.URL;
-              var txt = 'Give the following link to your friends so they can join your game: ';
-              $('#lobby-how-to-play').text(txt);
-              $('#oh-el').css({'text-align': 'center', 'font-size':'22px', 'background': 'white', 'color': 'black'}).text(link);
-            }, 200);
-            $scope.modalShown = true;
-          }
+        console.log('updating url');
+        $location.search({ game: game.gameID });
+        if (!$scope.modalShown) {
+          game.gameOwner = window.user._id;
+          setTimeout(() => {
+            const link = document.URL;
+            const txt = 'Give the following link to your friends so they can join your game: ';
+            $('#lobby-how-to-play').text(txt);
+            $('#oh-el').css({ 'text-align': 'center', 'font-size': '22px', background: 'white', color: 'black' }).text(link);
+          }, 200);
+          $scope.modalShown = true;
         }
       }
-    });
-
-    if ($location.search().game && !(/^\d+$/).test($location.search().game)) {
-      console.log('joining custom game');
-      game.joinGame('joinGame',$location.search().game);
-    } else if ($location.search().custom) {
-      game.joinGame('joinGame',null,true);
-    } else {
-      game.joinGame();
+      if (game.gameOwner === window.user._id) {
+        $scope.showFindUsersButton = true;
+      } else {
+        $scope.showFindUsersButton = false;
+      }
     }
+  });
 
+  if ($location.search().game && !(/^\d+$/).test($location.search().game)) {
+    console.log('joining custom game');
+    // console.log(game.players.length, game.playerMaxLimit, 'One');
+    if (game.players.length >= game.playerMaxLimit) {
+      $scope.gameInviteMessage = 'Maximum number of players for this game has been reached. You\'ll be redirected';
+      $('#gameModal').modal(); // show message and redirect the user
+      console.log('Max reached 1');
+    } else {
+      game.joinGame('joinGame', $location.search().game);
+    }
+  } else if ($location.search().custom) {
+    if (game.players.length >= game.playerMaxLimit) {
+      $scope.gameInviteMessage = 'Maximum number of players for this game has been reached. You\'ll be redirected';
+      $('#gameModal').modal(); // show message and redirect the user
+      console.log('Max reached 2');
+    } else {
+      console.log('third');
+      game.joinGame('joinGame', null, true);
+    }
+  } else {
+    console.log('ready to join');
+    game.joinGame();
+  }
 }]);
