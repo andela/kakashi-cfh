4c26aec49360cca21182c71137f35616e28976fd
Tracy Ezebuike <tracy.ezebuike@andela.com>
2017-03-22 23:28:34 +0100

    chore(index.js): add angular functions for user signup and signin
    The functions will receive the response from the http post and set the token or display an error message.

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,24 +1,65 @@
 angular.module('mean.system')
-.controller('IndexController', ['$scope', 'Global', '$location', 'socket', 'game', 'AvatarService', function ($scope, Global, $location, socket, game, AvatarService) {
+.controller('IndexController', ['$scope', '$location', '$window', '$http', 'Global', 'socket', 'game', 'AvatarService', 'Users',
+  ($scope, $location, $window, $http, Global, socket, game, AvatarService, Users) => {
     $scope.global = Global;
+    $scope.signupErrMsg = '';
+    $scope.signinErrMsg = '';
 
-    $scope.playAsGuest = function() {
+    if ($window.localStorage.getItem('token')) {
+      $scope.showOptions = false;
+    } else {
+      $scope.showOptions = true;
+    }
+
+    $scope.signup = () => {
+      Users.signup($scope.name, $scope.email, $scope.password).then((response) => {
+        if (response.success) {
+          $window.localStorage.setItem('token', response.token);
+          $location.path('/');
+        } else {
+          $scope.signupErrMsg = response.message;
+        }
+      }, (err) => {
+        $scope.showError();
+        $scope.error = err;
+      });
+    };
+
+    $scope.signin = () => {
+      // return console.log($scope.email, $scope.password);
+      Users.signin($scope.email, $scope.password).then((response) => {
+        if (response.success) {
+          $window.localStorage.setItem('token', response.token);
+          $location.path('/');
+        } else {
+          $scope.signinErrMsg = response.message;
+        }
+      }, (err) => {
+        $scope.showError();
+        $scope.error = err;
+      });
+    };
+
+    $scope.logout = () => {
+      $window.localStorage.removeItem('token');
+      $scope.showOptions = true;
+      $location.path('/');
+    };
+
+    $scope.playAsGuest = () => {
       game.joinGame();
       $location.path('/app');
     };
 
-    $scope.showError = function() {
+    $scope.showError = () => {
       if ($location.search().error) {
         return $location.search().error;
-      } else {
-        return false;
       }
     };
 
     $scope.avatars = [];
     AvatarService.getAvatars()
-      .then(function(data) {
+      .then((data) => {
         $scope.avatars = data;
       });
-
-}]);
+  }]);
