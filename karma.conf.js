00587448c9b100c2dbf37c080a3fa57d4d8edd60
andela-judensi <joshua.udensi@andela.com>
2017-03-29 19:02:01 +0100

    Feature(karma.conf.js): add dependencies for test
    angular.js
    angular-mocks
    angular-resource
    angular-route
    ui-bootstrap
    route
    
    141404651

--- karma.conf.js
+++ karma.conf.js
@@ -3,9 +3,18 @@ module.exports = (config) => {
     basePath: '',
     frameworks: ['jasmine'],
     files: [
-      'test/client/**/*Spec.js'
+      'public/lib/angular/angular.js',
+      'public/lib/angular-mocks/angular-mocks.js',
+      'public/lib/angular-cookies/angular-cookies.js',
+      'public/lib/angular-resource/angular-resource.js',
+      'public/lib/angular-route/angular-route.js',
+      'public/lib/angular-bootstrap/ui-bootstrap.min.js',
+      'public/lib/angular-ui-utils/modules/route/route.js',
+      'public/js/**/*.js',
+      'test/client/gameinviteSpec.js',
     ],
     exclude: [
+      'public/js/main.js'
     ],
     coverageReporter: {
       type: 'lcov',
@@ -27,6 +36,6 @@ module.exports = (config) => {
     },
     browsers: process.env.TRAVIS ? ['Chrome_travis_ci'] : ['Chrome'],
     singleRun: false,
-    concurrency: Infinity,
+    concurrency: Infinity
   });
 };
