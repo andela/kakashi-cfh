92d7329ca97a95b48fd4b63f1f431b85c8f6f268
andela-judensi <joshua.udensi@andela.com>
2017-03-29 19:37:59 +0100

    feature(model.js): user unit test unit test for creating and saving users
    141404651

---
+++ test/backend/user/model.js
@@ -0,0 +1,47 @@
+process.env.NODE_ENV = 'test';
+
+const should = require('should');
+const app = require('../../../server');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User');
+
+let user;
+
+describe('<Unit Test>', () => {
+  describe('Model User:', () => {
+    before((done) => {
+      user = new User({
+        name: 'Full name',
+        email: 'user@test.com',
+        username: 'user',
+        password: 'password'
+      });
+
+      done();
+    });
+
+    describe('Method Save', () => {
+      it('should be able to save whithout problems', (done) => {
+        user.save((err) => {
+          should.not.exist(err);
+          done();
+        });
+      });
+
+      it('should be able to show an error when try to save witout name', (done) => {
+        user.name = '';
+        user.save((err) => {
+          should.exist(err);
+          done();
+        });
+      });
+    });
+
+    after((done) => {
+      User.remove({}, () => {
+        done();
+      });
+    });
+  });
+});
