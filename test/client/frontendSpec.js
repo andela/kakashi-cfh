f723eda0c075779c3539440d2e75b74fe3850e99
andela-judensi <joshua.udensi@andela.com>
2017-04-03 13:54:13 +0100

    feature(frontendSpec.js): add frontend test add jasmine test for angular game controller describe('The game controller', () => {   let ;   let ;   let controller;   const game = {};   const Users = {};   let factory = {};   let http;
      beforeEach(() => {
        module('mean');
        // module('mean.system');
        inject(() => {
           = .get('');
           = .();
          controller = .get('')('GameController', { , game });
          factory =  .get('Users');
        });
      });
    
      describe('Some functions', () => {
        it('should return true', () => {
          expect(typeof .findUsers).toEqual('function');
        });
      });
    
      describe('Some functions', () => {
        it('should return true', () => {
          expect(Array.isArray(.usersInvited)).toBeTruthy();
        });
    
        it('should return true', () => {
          expect(Array.isArray(factory.usersInvited)).toBeTruthy();
        });
      });
    });

--- test/client/frontendSpec.js
+++ test/client/frontendSpec.js
@@ -1,5 +1,36 @@
-describe('Users factory', function () {
-  it('has a dummy spec to test 2 + 2', function () {
-    expect(2 + 2).toEqual(4);
+describe('The game controller', () => {
+  let $rootScope;
+  let $scope;
+  let controller;
+  const game = {};
+  const Users = {};
+  let factory = {};
+  let http;
+
+  beforeEach(() => {
+    module('mean');
+    // module('mean.system');
+    inject(($injector) => {
+      $rootScope = $injector.get('$rootScope');
+      $scope = $rootScope.$new();
+      controller = $injector.get('$controller')('GameController', { $scope, game });
+      factory =  $injector.get('Users');
+    });
+  });
+
+  describe('Some functions', () => {
+    it('should return true', () => {
+      expect(typeof $scope.findUsers).toEqual('function');
+    });
+  });
+
+  describe('Some functions', () => {
+    it('should return true', () => {
+      expect(Array.isArray($scope.usersInvited)).toBeTruthy();
+    });
+
+    it('should return true', () => {
+      expect(Array.isArray(factory.usersInvited)).toBeTruthy();
+    });
   });
 });
