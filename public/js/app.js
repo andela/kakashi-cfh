622c6ed2a2b10a1813f389880b643a4fc3b903eb
andela-judensi <joshua.udensi@andela.com>
2017-04-05 15:21:18 +0100

    feature(app.js): add ngSanitize
    add ngSanitize dependency
    
    [141404655]

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-angular.module('mean', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.directives'])
+angular.module('mean', ['ngCookies', 'ngSanitize', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.directives'])
   .config(['$routeProvider',
     function mean($routeProvider) {
       $routeProvider
