487548a3548417cf084de63c14dc23d9e4465978
andela-judensi <joshua.udensi@andela.com>
2017-04-05 19:22:27 +0100

    feature(all.js): fix eslint
    remove trailing spaces and unused var
    
    [141404651]

--- config/env/all.js
+++ config/env/all.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
-const rootPath = path.normalize(__dirname + '/../..');
-const keys = rootPath + '/keys.txt';
+const rootPath = path.normalize(`${__dirname}/../..`);
+// const keys = `${rootPath}/keys.txt`;
 
-const database = (process.NODE_ENV === 'test' ? process.env.MONGO_TEST = 'test' : process.env.MONGOHQ_URL); 
+const database = (process.NODE_ENV === 'test' ? process.env.MONGO_TEST = 'test' : process.env.MONGOHQ_URL);
 module.exports = {
   root: rootPath,
   port: process.env.PORT || 3000,
