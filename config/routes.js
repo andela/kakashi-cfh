4b8938b527705cf93930f58ef4c9cc8ae9c6c33c
andela-judensi <joshua.udensi@andela.com>
2017-04-11 13:18:53 +0100

    faature(routes.js): create record routes
    record game on start and update on game end routes
    [#141404665]

--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,8 @@ module.exports = (app, passport) => {
   app.post('/users/sendinvite', users.sendInvites);
 
   // game start route
-  app.post('/api/games/:id/start', users.isAuthenticated, game.record);
+  app.post('/api/games/:id/start', users.isAuthenticated, game.saveRecord);
+  app.post('/api/games/record/end', users.isAuthenticated, game.updateRecord);
 
     // Setting the facebook oauth routes
   app.get('/auth/facebook', passport.authenticate('facebook', {
