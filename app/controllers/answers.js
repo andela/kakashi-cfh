95b53b70fbd5d93e63260eca491afebb7da745dd
Jedidiah Omadoye <jedidiahomadoye@Andelas-MacBook-Pro.local>
2017-04-12 17:33:21 +0100

    feature(answers.js) add region query for answers
    141404667

--- app/controllers/answers.js
+++ app/controllers/answers.js
@@ -1,55 +1,58 @@
 /**
  * Module dependencies.
  */
-var mongoose = require('mongoose'),
-    async = require('async'),
-    Answer = mongoose.model('Answer'),
-    _ = require('underscore');
+const mongoose = require('mongoose');
+
+const Answer = mongoose.model('Answer');
+const _ = require('underscore');
 
 
 /**
  * Find answer by id
  */
-exports.answer = function(req, res, next, id) {
-    Answer.load(id, function(err, answer) {
-        if (err) return next(err);
-        if (!answer) return next(new Error('Failed to load answer ' + id));
-        req.answer = answer;
-        next();
-    });
+exports.answer = (req, res, next, id) => {
+  Answer.load(id, (err, answer) => {
+    if (err) return next(err);
+    if (!answer) return next(new Error(`Failed to load answer ${id}`));
+    req.answer = answer;
+    next();
+  });
 };
 
 /**
  * Show an answer
  */
-exports.show = function(req, res) {
-    res.jsonp(req.answer);
+exports.show = (req, res) => {
+  res.jsonp(req.answer);
 };
 
 /**
  * List of Answers
  */
-exports.all = function(req, res) {
-    Answer.find({official:true}).select('-_id').exec(function(err, answers) {
-        if (err) {
-            res.render('error', {
-                status: 500
-            });
-        } else {
-            res.jsonp(answers);
-        }
-    });
+exports.all = (req, res) => {
+  Answer.find({ official: true }).select('-_id').exec((err, answers) => {
+    if (err) {
+      res.render('error', {
+        status: 500
+      });
+    } else {
+      res.jsonp(answers);
+    }
+  });
 };
 
 /**
  * List of Answers (for Game class)
  */
-exports.allAnswersForGame = function(cb) {
-    Answer.find({official:true}).select('-_id').exec(function(err, answers) {
-        if (err) {
-            console.log(err);
-        } else {
-            cb(answers);
-        }
-    });
+exports.allAnswersForGame = (region, cb) => {
+  const query = (region === 'general') ?
+                { official: true } :
+                { official: true, location: region };
+  Answer.find(query).select('-_id').exec((err, answers) => {
+    if (err) {
+      console.log(err);
+    } else {
+      cb(answers);
+    }
+  });
 };
