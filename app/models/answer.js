e77ff3be811619725912dbc197bb25721a98437c
Jedidiah Omadoye <jedidiahomadoye@Andelas-MacBook-Pro.local>
2017-04-18 11:38:31 +0100

    feature(app/controllers/answers.js) fix eslint issues
    141404667

--- app/models/answer.js
+++ app/models/answer.js
@@ -1,44 +1,43 @@
 /**
  * Module dependencies.
  */
-var mongoose = require('mongoose'),
-    config = require('../../config/config'),
-    Schema = mongoose.Schema;
+const mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
 
 /**
  * Answer Schema
  */
-var AnswerSchema = new Schema({
-    id: {
-        type: Number
-    },
-    text: {
-        type: String,
-        default: '',
-        trim: true
-    },
-    official: {
-        type: Boolean
-    },
-    expansion: {
-        type: String,
-        default: '',
-        trim: true
-    },
-    location: {
-        type: String
-    }
+const AnswerSchema = new Schema({
+  id: {
+    type: Number
+  },
+  text: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  official: {
+    type: Boolean
+  },
+  expansion: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  location: {
+    type: String
+  }
 });
 
 /**
  * Statics
  */
 AnswerSchema.statics = {
-    load: function(id, cb) {
-        this.findOne({
-            id: id
-        }).select('-_id').exec(cb);
-    }
+  load: (id, cb) => {
+    this.findOne({
+      id
+    }).select('-_id').exec(cb);
+  }
 };
 
 mongoose.model('Answer', AnswerSchema);
