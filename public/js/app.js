993ccc29d86560bf8e2bbd12ffe38468bd9a5e7c
Tracy Ezebuike <tracy.ezebuike@andela.com>
2017-04-19 09:31:16 +0100

    feature(app.js): add welcome.html to template
    add welcome.html to template
    #141404659

--- public/js/app.js
+++ public/js/app.js
@@ -23,6 +23,9 @@ angular.module('mean', ['ngCookies', 'ngSanitize', 'ngResource', 'ngRoute', 'ui.
         .when('/choose-avatar', {
           templateUrl: '/views/choose-avatar.html'
         })
+        .when('/welcome', {
+          templateUrl: '/views/welcome.html'
+        })
         .otherwise({
           redirectTo: '/'
         });
